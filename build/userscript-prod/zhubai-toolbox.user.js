92dad0814ecbd470ab458c57762589016469fd07
pipecraft <pipecraft.net@gmail.com>
2023-04-14 10:17:28 +0800

    First commit of zhubai-toolbox

---
+++ build/userscript-prod/zhubai-toolbox.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name                 Zhubai Toolbox 🧰
+// @name:zh-CN           竹白工具箱 🧰
+// @namespace            https://github.com/utags/zhubai-toolbox
+// @homepageURL          https://github.com/utags/zhubai-toolbox#readme
+// @supportURL           https://github.com/utags/zhubai-toolbox/issues
+// @version              0.1.0
+// @description          Tools for Zhubai creators.
+// @description:zh-CN    为竹白创作者的工具箱，包括订阅者信息导出，Markdown 编辑器等功能。更多功能欢迎交流。
+// @icon                 https://zhubai.love/favicon.png
+// @author               Pipecraft
+// @license              MIT
+// @match                https://*/*
+// @match                http://*/*
+// ==/UserScript==
+//
+;(() => {
+  "use strict"
+})()
